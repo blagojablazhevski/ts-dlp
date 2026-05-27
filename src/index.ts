a14d68502afecab02e9d0d6459a8674fb8c6adf1
Blagoja Blazhevski <blagoja@blazhevski.com>
2026-05-27 17:59:58 +0200

    feat(index): re-export YtDlpError and YtDlpNotFoundError along with types from errors and types modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import {DownloadBuilder} from "./download.js";
 
+export {YtDlpError, YtDlpNotFoundError} from "./errors.js";
+export type {Resolution, AudioCodec, VideoExt, OutputFields} from "./types.js";
+
 export function download(url: string): DownloadBuilder {
 	return new DownloadBuilder(url);
 }
