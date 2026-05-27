90ddd1381840330ad90a8e1ec1448ed5d1405c84
Blagoja Blazhevski <blagoja@blazhevski.com>
2026-05-27 12:27:19 +0200

    feat: define Resolution and OutputFields types in types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,33 @@
-export {};
+type Resolution =
+	| "144p"
+	| "240p"
+	| "360p"
+	| "480p"
+	| "720p"
+	| "1080p"
+	| "1440p"
+	| "2160p"
+	| (string & {});
+
+type OutputFields = {
+	title: string;
+	ext: string;
+	id: string;
+	uploader: string;
+	uploader_id: string;
+	channel: string;
+	upload_date: string;
+	release_date: string;
+	epoch: string;
+	height: string;
+	width: string;
+	resolution: string;
+	fps: string;
+	vcodec: string;
+	acodec: string;
+	duration_string: string;
+	playlist_title: string;
+	playlist_index: string;
+	autonumber: string;
+};
+export type {Resolution, OutputFields};
