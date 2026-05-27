a235a191b4861a2d8449ca2d2c1bdd49e6b9787e
Blagoja Blazhevski <blagoja@blazhevski.com>
2026-05-27 18:57:54 +0200

    feat: export RunResult type from internal runner module

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {DownloadBuilder} from "./download.js";
 
 export {YtDlpError, YtDlpNotFoundError} from "./errors.js";
 export type {Resolution, AudioCodec, VideoExt, OutputFields} from "./types.js";
+export type {RunResult} from "./internal/runner.js";
 
 export function download(url: string): DownloadBuilder {
 	return new DownloadBuilder(url);
